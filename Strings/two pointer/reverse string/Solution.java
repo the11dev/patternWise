de13d076351479a5f588d09851bb97d6aa16eec6
Vaishanvi Mishra <vaishnavim1105@gmail.com>
2026-05-01 12:32:18 +0530

    344 reverse string

---
+++ Strings/two pointer/reverse string/Solution.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-string/description/
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+
+            i++;
+            j--;
+        }
+    }
+}
